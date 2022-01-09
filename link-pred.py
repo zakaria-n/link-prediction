97c07b7cfb65b877ef82b44f59cf6862d2bbab62
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2022-01-09 02:21:05 +0100

    3 nv scores and all set to clusterise

--- link-pred.py
+++ link-pred.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
-labeled_dataset = pd.read_pickle('cora-labelled.pkl', compression='infer')
+labeled_dataset = pd.read_pickle('blogcat-labelled.pkl', compression='infer')
 labeled_sample = labeled_dataset.sample(1000000)
 
 training_set, validation_set = train_test_split(labeled_sample, test_size = 0.5, random_state = 21)
@@ -16,19 +16,19 @@ Y_val = validation_set[["edge"]]
 
 print("Splitting...")
 X_train_split = pd.concat([pd.DataFrame(X_train['node1'].to_list()),pd.DataFrame(X_train['node2'].to_list())], axis=1)
-with open('X_train_split.pkl', 'wb') as f:
-    pickle.dump(X_train_split,f)
+# with open('airport-X_train_split.pkl', 'wb') as f:
+#     pickle.dump(X_train_split,f)
 
 logisticRegr = LogisticRegression()
 logisticRegr.fit(X_train_split, Y_train.values.ravel())
 
 X_val_split = pd.concat([pd.DataFrame(X_val['node1'].to_list()),pd.DataFrame(X_val['node2'].to_list())], axis=1)
-with open('X_val_split.pkl', 'wb') as f:
-    pickle.dump(X_val_split,f)
+# with open('airport-X_val_split.pkl', 'wb') as f:
+#     pickle.dump(X_val_split,f)
 
 Y_pred = logisticRegr.predict(X_val_split)
 
-with open("cora_n2v_scores.txt", "w") as f:
+with open("blogcat_n2v_scores.txt", "w") as f:
     # Writing data to a file
     f.write("Macro F1: ")
     f.write(str(f1_score(Y_val, Y_pred, average='macro')))
--- preprocess.py
+++ preprocess.py
@@ -7,14 +7,15 @@ columns = ["node_id"]
 for i in range(128):
     columns.append("dim"+str(i+1))
 
-cora_emb = pd.read_csv('embeddings/node2vec/cora.emb', delim_whitespace=True, names=columns).sort_values(by=['node_id'], ascending=True)
+emb = pd.read_csv('embeddings/node2vec/blogcat.emb', delim_whitespace=True, names=columns).sort_values(by=['node_id'], ascending=True)
+emb = emb.sample(2710)
 
 dimensions = []
 for i in range(128):
     dimensions.append("dim"+str(i+1))
 
 nodes = []
-for index, row in cora_emb.iterrows():
+for index, row in emb.iterrows():
     nodes.append(row["node_id"])
 
 pairs = []
@@ -26,12 +27,12 @@ pairs = pd.DataFrame.from_records(pairs)
 pairs.columns=["node1", "node2", "edge"]
 
 node_embeddings = {}
-for index, row in cora_emb.iterrows():
+for index, row in emb.iterrows():
     node_embeddings[row["node_id"]] = [row[dim] for dim in dimensions]
 
-cora_edges = pd.read_csv('edgelists/cora.edgelist', delim_whitespace=True, names=["node1", "node2"])
+edges = pd.read_csv('edgelists/blogcat.edgelist', delim_whitespace=True, names=["node1", "node2"])
 
-for index_edge, row_edge in tqdm(cora_edges.iterrows(), total=cora_edges.shape[0]):
+for index_edge, row_edge in tqdm(edges.iterrows(), total=edges.shape[0]):
     pair_row = pairs.loc[(pairs['node1'] == row_edge['node1']) & (pairs['node2'] == row_edge['node2'])]
     pairs.loc[pair_row.index, "edge"] = 1
 
@@ -40,5 +41,5 @@ labeled_dataset = pairs
 labeled_dataset['node1'] = labeled_dataset['node1'].map(node_embeddings)
 labeled_dataset['node2'] = labeled_dataset['node2'].map(node_embeddings)
 
-with open('cora-labelled.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+with open('blogcat-labelled.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
     pickle.dump(labeled_dataset,f)
